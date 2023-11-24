b8e3ef3b2573db67130062c2c37dc3afea571db2
Bhanumahesh <bhanumahesh70@gmail.com>
2023-11-23 20:38:03 -0600

    Modified client view and client controller to add and display All songs

--- src/controllers/ClientController.java
+++ src/controllers/ClientController.java
@@ -4,11 +4,19 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import dao.DBConnect;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import models.Song;
+import models.SongModel;
 
 
 public class ClientController {
@@ -20,11 +28,13 @@ public class ClientController {
 	private Pane panel1;
 	
 	@FXML
+	private Pane allsongs_panel;
+	
+	@FXML
 	private TextField playlistName_text;
 	
 	@FXML
 	private TextField songName_text;
-	
 	
 	// Declare DB objects
 		DBConnect conn = null;
@@ -36,9 +46,16 @@ public class ClientController {
 		conn = new DBConnect();
 	}
 	
+	public void displaySongs() {
+		playlist_panel.setVisible(false);
+		panel1.setVisible(false);
+		allsongs_panel.setVisible(true);
+	}
+	
 	public void createPlaylist_click() {
 		playlist_panel.setVisible(true);
 		panel1.setVisible(false);
+		allsongs_panel.setVisible(false);
 	}
 	
 	public void createPlaylist() {
@@ -76,5 +93,93 @@ public class ClientController {
 				se.printStackTrace();
 				}
 	}
+/*
+	
+	@FXML
+    private TableView songs_tableview;
 
+    @FXML
+    private TableColumn title_column;
+
+    @FXML
+    private TableColumn album_column;
+
+    @FXML
+    private TableColumn artist_column;
+	public void displaySongs() {
+		System.out.println("Displaying all songs from songs table...");
+		try {
+			stmt = conn.getConnection().createStatement();
+			String sql = null;
+			sql= "SELECT * FROM beatmusic_songs";
+			myRs = stmt.executeQuery(sql);
+			
+			
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	*/
+	
+	@FXML
+    private TableView<SongModel> songs_tableview;
+
+    @FXML
+    private TableColumn<SongModel, String> title_column;
+
+    @FXML
+    private TableColumn<SongModel, String> artist_column;
+
+    @FXML
+    private TableColumn<SongModel, String> album_column;
+
+    @FXML
+    private TableColumn<SongModel, Integer> duration_column;
+    public void initialize() {
+    	title_column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
+    	artist_column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getArtist()));
+    	album_column.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAlbum()));
+    	duration_column.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getDuration()).asObject());
+
+
+        // Connect to the database and retrieve data
+        List<SongModel> songs = fetchDataFromDatabase();
+
+        // Populate the TableView with data
+        songs_tableview.getItems().addAll(songs);
+        System.out.println("Displaying all songs");
+    }
+
+    private List<SongModel> fetchDataFromDatabase() {
+        List<SongModel> songs = new ArrayList<>();
+
+        // TODO: Connect to your database
+        try { 
+        	
+        	System.out.println("Fetching all songs from songs table in database");
+        	stmt = conn.getConnection().createStatement();
+			String sql = null;
+			sql= "SELECT * FROM beatmusic_songs";
+			myRs = stmt.executeQuery(sql);
+
+            while (myRs.next()) {
+                String title = myRs.getString("title");
+                String artist = myRs.getString("artist");
+                String album = myRs.getString("album");
+                int duration = myRs.getInt("duration");
+
+                songs.add(new SongModel(title, artist, album,duration));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return songs;
+    }
+	
+	
 }
---
+++ src/models/Song.java
@@ -0,0 +1,27 @@
+package models;
+
+
+
+public class Song {
+    private final String title;
+    private final String artist;
+    private final String album;
+
+    public Song(String title, String artist, String album) {
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+}
