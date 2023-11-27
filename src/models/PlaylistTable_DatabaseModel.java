826f748051327edbdb62cbd8acddac5a3a6a99cd
Bhanumahesh <bhanumahesh70@gmail.com>
2023-11-26 20:22:43 -0600

    added code for creating playlist and adding songs to them

--- src/models/PlaylistTable_DatabaseModel.java
+++ src/models/PlaylistTable_DatabaseModel.java
@@ -10,36 +10,36 @@ import java.util.List;
 
 public class PlaylistTable_DatabaseModel {
 
-	public static List<PlaylistModel> fetchPlaylistsFromDatabase(int userId)  {
+	public static List<PlaylistModel> fetchPlaylistsFromDatabase(int userId) {
 		List<PlaylistModel> playlists = new ArrayList<>();
 
-		try  {
-			
-			PreparedStatement stmt = DBConnect.getConnection().prepareStatement("SELECT * FROM beatmusic_playlist WHERE user_id = ?");
+		try {
+
+			PreparedStatement stmt = DBConnect.getConnection()
+					.prepareStatement("SELECT * FROM beatmusic_playlist WHERE user_id = ?");
 
 			stmt.setInt(1, userId);
 
 			ResultSet rs = stmt.executeQuery();
-				while (rs.next()) {
-					String playlistName = rs.getString("playlist_name");
-					int songId = rs.getInt("song_id");
+			while (rs.next()) {
+				String playlistName = rs.getString("playlist_name");
+				int songId = rs.getInt("song_id");
 
-					boolean playlistPresent_inList = false;
-					for (PlaylistModel playlist : playlists) {
-						if (playlist.getPlaylistName().equals(playlistName)) {
-							playlist.addSongId(songId);
-							playlistPresent_inList = true;
-							break;
-						}
-					}
-					if (!playlistPresent_inList) {
-						List<Integer> songIds = new ArrayList<>();
-						songIds.add(songId);
-						playlists.add(new PlaylistModel(playlistName, userId, songIds));
+				boolean playlistPresent_inList = false;
+				for (PlaylistModel playlist : playlists) {
+					if (playlist.getPlaylistName().equals(playlistName)) {
+						playlist.addSongId(songId);
+						playlistPresent_inList = true;
+						break;
 					}
 				}
+				if (!playlistPresent_inList) {
+					List<Integer> songIds = new ArrayList<>();
+					songIds.add(songId);
+					playlists.add(new PlaylistModel(playlistName, userId, songIds));
+				}
 			}
-		 catch (SQLException e) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -56,5 +56,78 @@ public class PlaylistTable_DatabaseModel {
 
 		return songsFromPlaylist;
 	}
+	
+	
+	
+	public static void insertIntoPlaylistDatabase(String playlistName, int user_id, int song_id) {
+		try {
+			
+			String sql = "INSERT INTO beatmusic_playlist(playlist_name,user_id,song_id) values (?,?,?)";
+			PreparedStatement stmt = DBConnect.getConnection().prepareStatement(sql);
+			stmt.setString(1, playlistName);
+			stmt.setInt(2, user_id);
+			stmt.setInt(3, song_id);
+			stmt.executeUpdate();
+			stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void addSongToPlaylist(String playlistName, int songId, int userId) {
+		try {
+			
+			if(isPlaylistEmpty(playlistName,userId)) {
+				String sql = "UPDATE beatmusic_playlist SET song_id = ? WHERE playlist_name=? AND user_id =?";
+				PreparedStatement stmt = DBConnect.getConnection().prepareStatement(sql);
+				stmt.setInt(1, songId);
+				stmt.setString(2, playlistName);
+				stmt.setInt(3, userId);
+				stmt.executeUpdate();
+				stmt.close();
+			}else {
+				
+				insertIntoPlaylistDatabase(playlistName,songId,userId);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void createEmptyPlaylist(String playlistName, int user_id) {
+		int song_id =-1;
+		insertIntoPlaylistDatabase(playlistName,user_id,song_id);
+	}
+	
+	public static boolean isPlaylistEmpty(String playlistName,int userId) {
+		boolean isEmptyPlaylist = false;
+		try {
+			String sql = "SELECT * FROM beatmusic_playlist WHERE playlist_name=? AND user_id =?";
+			PreparedStatement stmt = DBConnect.getConnection().prepareStatement(sql);
+			stmt.setString(1, playlistName);
+			stmt.setInt(1, userId);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				int song_id = rs.getInt("song_id");
+				if(song_id==-1)
+				{
+					isEmptyPlaylist = true;		
+				}
+				else {
+					break;
+				}
+			}
+			stmt.close();
+			rs.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return isEmptyPlaylist;
+	}
+	
+	
+	
 
 }
--- src/models/SongTable_DatabaseModel.java
+++ src/models/SongTable_DatabaseModel.java
@@ -11,30 +11,30 @@ import java.sql.Statement;
 
 public class SongTable_DatabaseModel {
 
-    public static ObservableList<SongModel> fetchSongsFromDatabase() {
-        ObservableList<SongModel> songs = FXCollections.observableArrayList();
+	public static ObservableList<SongModel> fetchSongsFromDatabase() {
+		ObservableList<SongModel> songs = FXCollections.observableArrayList();
 
-        try  {
-        	PreparedStatement stmt = DBConnect.getConnection().prepareStatement("SELECT * FROM beatmusic_songs");
-            ResultSet rs = stmt.executeQuery();
+		try {
+			PreparedStatement stmt = DBConnect.getConnection().prepareStatement("SELECT * FROM beatmusic_songs");
+			ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()) {
-                String title = rs.getString("title");
-                String artist = rs.getString("artist");
-                String album = rs.getString("album");
-                int duration = rs.getInt("duration");
+			while (rs.next()) {
+				String title = rs.getString("title");
+				String artist = rs.getString("artist");
+				String album = rs.getString("album");
+				int duration = rs.getInt("duration");
 
-                songs.add(new SongModel(title, artist, album, duration));
-            }
-        } catch (Exception e) {
+				songs.add(new SongModel(title, artist, album, duration));
+			}
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-        return songs;
-    }
-    
-    public static SongModel fetchSongFromDatabase(int song_id) {
+		return songs;
+	}
+
+	public static SongModel fetchSongFromDatabase(int song_id) {
 		SongModel song = null;
 
 		// TODO: Connect to your database
@@ -42,7 +42,7 @@ public class SongTable_DatabaseModel {
 			Statement stmt = DBConnect.getConnection().createStatement();
 			String sql = null;
 			sql = "SELECT * FROM beatmusic_songs WHERE song_id=" + song_id;
-			 ResultSet myRs = stmt.executeQuery(sql);
+			ResultSet myRs = stmt.executeQuery(sql);
 
 			while (myRs.next()) {
 				String title = myRs.getString("title");
@@ -51,8 +51,8 @@ public class SongTable_DatabaseModel {
 				int duration = myRs.getInt("duration");
 
 				song = new SongModel(title, artist, album, duration);
-				
-				//conn.getConnection().close();
+
+				// conn.getConnection().close();
 			}
 			stmt.close();
 			myRs.close();
@@ -61,5 +61,23 @@ public class SongTable_DatabaseModel {
 		}
 
 		return song;
+	}
+	
+	public static void insertSongInToDatabase(SongModel song) {
+		try {
+			
+			String sql = "INSERT INTO beatmusic_songs (title, artist, album, duration) VALUES (?,?,?,?)";
+
+			PreparedStatement stmt = DBConnect.getConnection().prepareStatement(sql);
+			stmt.setString(1,song.getTitle());
+			stmt.setString(2, song.getArtist());
+			stmt.setString(3, song.getAlbum());
+			stmt.setInt(4, song.getDuration());
+			stmt.executeUpdate();
+			stmt.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
